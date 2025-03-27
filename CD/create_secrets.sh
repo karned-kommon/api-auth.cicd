a02418f70b35af02c54040c36f5eb44b2fa27108
Killian KOPP <killiankopp@gmail.com>
2025-03-27 13:02:10 +0100

    CI CD

--- CD/create_secrets.sh
+++ CD/create_secrets.sh
@@ -2,5 +2,4 @@ kubectl create secret generic keycloak-secret \
   --from-literal=KEYCLOAK_HOST="https://www.urlserveur.bzh" \
   --from-literal=KEYCLOAK_REALM="myrealm" \
   --from-literal=KEYCLOAK_CLIENT_ID="myid" \
-  --from-literal=KEYCLOAK_CLIENT_SECRET="mypassword" \
-  -n karned
+  --from-literal=KEYCLOAK_CLIENT_SECRET="mypassword"
--- README.md
+++ README.md
@@ -1,11 +1,2 @@
-# keycloack-k8s
+# keycloak.k8s
 
-## personnalisation
-```sh
-helm show values oci://registry-1.docker.io/bitnamicharts/keycloak
-```
-
-## installation
-```sh
-helm install keycloack oci://registry-1.docker.io/bitnamicharts/keycloak -f values.yaml
-```
